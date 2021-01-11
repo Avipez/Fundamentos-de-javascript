7cd0f7d92cf22db308b7bf2400806e3f4d1aec4e
Volta <dg_lf@hotmail.com>
2021-01-10 20:13:42 -0500

    manejo de errores con callbacks

--- clase 23.js
+++ clase 23.js
@@ -6,10 +6,27 @@ const onResponse = function(luke){
     console.log(`Hola soy ${luke.name}`)
 }
 
-function obtenerPersonaje(id){
+function obtenerPersonaje(id, callback){
     const url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`
-    $.get(url, opts, onResponse)
+    $.get(url, opts, onResponse).fail(function({
+        console.log("Sucedio un error y no se pudo conseguir el personaje")
+    }))
+
+    if(callback){
+        callback()
+    }
 }
 
 
-obtenerPersonaje(1)
+obtenerPersonaje(1, function(personaje) {
+    console.log(`Hola soy ${personaje.name}`)
+
+    obtenerPersonaje(2, function(personaje) {
+        console.log(`Hola soy ${personaje.name}`)
+
+        obtenerPersonaje(3, function(personaje) {
+            console.log(`Hola soy ${personaje.name}`)
+
+        })
+    })
+})
